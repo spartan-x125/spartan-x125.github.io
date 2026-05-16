b6eaa1475958974ef268a4778a24f80760f3ae00
spartan-x125 <Spartan_x@126.com>
2026-05-16 15:05:16 +0800

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   base:'/',
-  site: "https://spartan-x125.github.io",
-  outDir: "docs",
+  //site: "https://spartan-x125.github.io",
+  //outDir: "docs",
 });
